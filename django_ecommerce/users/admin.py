25d81d85d6b92a5e13766feec91f5bc49cc25093
Ksenofanex <ksenofanex@gmail.com>
2021-01-13 13:45:57 +0300

    add authentication and model files

---
+++ django_ecommerce/users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff',]
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'phone_number', )}
+         ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
