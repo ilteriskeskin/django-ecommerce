98be901371aa213ca977b5970fef45c918d2c4be
Ksenofanex <ksenofanex@gmail.com>
2021-01-13 15:31:11 +0300

    add product model, product CRUD and searching

--- django_ecommerce/django_ecommerce/urls.py
+++ django_ecommerce/django_ecommerce/urls.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+from products import views as ProductViews
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+
+    # Authentication.
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+
+    # Products.
+    path('products/', include('products.urls')),
+
+    # Searching.
+    path('search/', ProductViews.SearchResultView.as_view(), name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
