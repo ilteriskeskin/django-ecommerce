b565cfb8b1cd31f39c94dd338e81b3e081dc3fa5
burakyilmaz55 <burakyilmz17@gmail.com>
2021-01-14 13:23:39 +0300

    fazlalık temizliği

--- django_ecommerce/users/views.py
+++ django_ecommerce/users/views.py
@@ -1,57 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
-from django.contrib.auth.forms import PasswordResetForm
-from django.contrib.auth.models import User
-from django.template.loader import render_to_string
-from django.db.models.query_utils import Q
-from django.utils.http import urlsafe_base64_encode
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_bytes
 
-
-def password_reset_request(request):
-    if request.method == "POST":
-        password_reset_form = PasswordResetForm(request.POST)
-        if password_reset_form.is_valid():
-            data = password_reset_form.cleaned_data["email"]
-            associated_users = User.objects.filter(Q(email=data))
-            if associated_users.exists():
-                for user in associated_users:
-                    subject = "Password Reset Requested"
-                    email_template_name = (
-                        "registration/password_reset_email.txt"
-                    )
-                    c = {
-                        "email": user.email,
-                        "domain": "127.0.0.1:8000",
-                        "site_name": "Website",
-                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                        "user": user,
-                        "token": default_token_generator.make_token(user),
-                        "protocol": "http",
-                    }
-                    email = render_to_string(email_template_name, c)
-                    try:
-                        send_mail(
-                            subject,
-                            email,
-                            "admin@example.com",
-                            [user.email],
-                            fail_silently=False,
-                        )
-                    except BadHeaderError:
-                        return HttpResponse("Invalid header found.")
-                    return redirect("/password_reset/done/")
-    password_reset_form = PasswordResetForm()
-    return render(
-        request=request,
-        template_name="registration/password_reset_form.html",
-        context={"password_reset_form": password_reset_form},
-    )
 
 
 class SignUpView(CreateView):
