b1a9f1a4f79508f5c32d0fcb5b8edfa59f9fd174
Ksenofanex <ksenofanex@gmail.com>
2021-01-16 12:21:54 +0300

    change detail urls from pk to slug

--- django_ecommerce/products/models.py
+++ django_ecommerce/products/models.py
@@ -8,11 +8,11 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Product Price')
     image = models.ImageField(upload_to='media/', verbose_name='Product Image')
     stock = models.IntegerField(verbose_name='Product Stock')
-    slug = models.SlugField(verbose_name='Product Slug')
+    slug = models.SlugField(verbose_name='Product Slug', unique=True)
     # category = models.ForeignKey()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('product-detail', kwargs={'pk': str(self.pk)})
+        return reverse('product-detail', kwargs={'slug': str(self.slug)})
